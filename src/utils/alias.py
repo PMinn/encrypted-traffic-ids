0521b2b43ba2d4f47f15f456d33e611cc3096553
pminn <p.min.developer@gmail.com>
2025-12-07 03:49:17 +0800

    refactor: 移除 alias.json 檔案並簡化路徑別名處理邏輯

--- src/utils/alias.py
+++ src/utils/alias.py
@@ -1,27 +1,9 @@
 from pathlib import Path, PurePath
-import json
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent.parent
 
 project_root: Path = get_project_root()
-def get_alias() -> dict:
-    """
-        定義路徑別名
-        Args:
-            None
-        Returns:
-            dict: 路徑別名字典
-    """
-    with open(project_root / "config" / "alias.json", "r") as f:
-        alias = json.load(f)
-    for key, value in alias.items():
-        if value.startswith("/"):
-            value = value[1:]
-        alias[key] = project_root.joinpath(PurePath(value))
-    return alias
-
-alias: dict = get_alias()
 
 def a2p(path: str) -> Path:
     """
@@ -31,7 +13,6 @@ def a2p(path: str) -> Path:
         Returns:
             Path: 轉換後的實際路徑
     """
-    for key, value in alias.items():
-        if path.startswith(key):
-            return value.joinpath(PurePath(path[len(key):]))
+    if path.startswith("@/"):
+        return project_root.joinpath(PurePath(path[2:]))
     raise ValueError(f"Path '{path}' does not start with any known alias.")
